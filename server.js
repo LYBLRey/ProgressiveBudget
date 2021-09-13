66d065df4d5abe746e93ff54ea788e24e2e1db70
Sareybodd Chhorn <sareyboddc@gmail.com>
2021-09-12 17:46:52 -0700

    url parser

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require("express")
+const logger = require("morgan")
+const mongoose = require("mongoose")
+const compression = require("compression")
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
 
-const app = express();
+const app = express()
 
-app.use(logger("dev"));
+app.use(logger("dev"))
 
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(compression())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.use(express.static("public"));
+app.use(express.static("public"))
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
-});
+})
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api.js"))
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+  console.log(`App running on port ${PORT}!`)
+})
